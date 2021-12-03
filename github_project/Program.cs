05748e77d6cb7d04944e552d21b38d9d50c4b9e7
SavoeunVanda <voeunvanda150@gmail.com>
2021-12-04 06:21:35 +0700

    Add project files.

---
+++ github_project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, Teacher!");
+            Console.ReadKey();
+        }
+    }
+}
